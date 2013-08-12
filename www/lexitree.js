382b8bca43d3ac8870990a315fd5114110edd2e1
Lau Llobet Payas <lau.llobet.payas@gmail.com>
2013-08-12 13:48:03 +0200

    db prepopulated loaded i accesible per la app phonegap en android

--- www/lexitree.js
+++ www/lexitree.js
@@ -97,35 +97,47 @@ $(document).ready(function() {
 		 
     	word_list.page++;
     	load_pinit();
+    	    	
     	
-
-    	console.log("PRVANDO 000");
-    	alert("Provado 123");
-    	console.log("PRVANDO 456");
     	
+/*
+ 	   var db = window.sqlitePlugin.openDatabase("Database", "1.0", "Demo", -1);
+                    
+                    alert("opened?");
+                    
+ 	   db.transaction(function(tx) {
+ 	     tx.executeSql('DROP TABLE IF EXISTS test_table');
+ 	     tx.executeSql('CREATE TABLE IF NOT EXISTS test_table (id integer primary key, data text, data_num integer)');
+ 	     tx.executeSql("INSERT INTO test_table (data, data_num) VALUES (?,?)", ["test", 100], function(tx, res) {
+ 	     console.log("insertId: " + res.insertId + " -- probably 1"); // check #18/#38 is fixed
+ 	     alert("insertId: " + res.insertId + " -- should be valid");
 
-    	   var db = window.sqlitePlugin.openDatabase("Database", "1.0", "Demo", -1);
-                       
-                       alert("opened?");
-                       
-    	   db.transaction(function(tx) {
-    	     tx.executeSql('DROP TABLE IF EXISTS test_table');
-    	     tx.executeSql('CREATE TABLE IF NOT EXISTS test_table (id integer primary key, data text, data_num integer)');
-    	     tx.executeSql("INSERT INTO test_table (data, data_num) VALUES (?,?)", ["test", 100], function(tx, res) {
-    	     console.log("insertId: " + res.insertId + " -- probably 1"); // check #18/#38 is fixed
-    	     alert("insertId: " + res.insertId + " -- should be valid");
-
-    	       db.transaction(function(tx) {
-    	         tx.executeSql("SELECT data_num from test_table;", [], function(tx, res) {
-    	           console.log("res.rows.length: " + res.rows.length + " -- should be 1");
-    	           alert("res.rows.item(0).data_num: " + res.rows.item(0).data_num + " -- should be 100");
+ 	       db.transaction(function(tx) {
+ 	         tx.executeSql("SELECT data_num from test_table;", [], function(tx, res) {
+ 	           console.log("res.rows.length: " + res.rows.length + " -- should be 1");
+ 	           alert("res.rows.item(0).data_num: " + res.rows.item(0).data_num + " -- should be 100");
+ 	         });
+ 	       });
+ 	     }, function(e) {
+ 	    	 alert("ERROR PACHANGA");
+ 	       console.log("ERROR: " + e.message);
+ 	     });
+ 	   });
+ 	   
+  */
+  			var db = window.sqlitePlugin.openDatabase("new_lexitree", "1.0", "new_lexitree.db", -1);
+  
+  
+            alert("opened?");
+    	    
+            db.transaction(function(tx) {
+    	         
+            	alert("selecting");
+            	tx.executeSql("SELECT lemma,pos,sensenum,synsetid,definition,sampleset  FROM dict WHERE lemma = 'want' ORDER BY pos,sensenum;", [], function(tx, res) {
+    	         //alert("res.rows.length: " + res.rows.length + " -- should be 1");
+    	         alert("def: " + res.rows.item(0).definition );
     	         });
     	       });
-    	     }, function(e) {
-    	    	 alert("ERROR PACHANGA");
-    	       console.log("ERROR: " + e.message);
-    	     });
-    	   });
     	
 
     	
