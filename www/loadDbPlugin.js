4e89d302938c9dfdf62f5350f18fc78e90924d77
Lau Llobet Payas <lau.llobet.payas@gmail.com>
2013-08-16 18:54:55 +0200

    falta meitat persistencia i clickable words

--- www/loadDbPlugin.js
+++ www/loadDbPlugin.js
@@ -12,10 +12,12 @@ if (userAgent.match(/android/i)) {
 }else if (userAgent.match(/ipad/i) || userAgent.match(/iphone/i) ) {
 	//alert("iphone");
 	document.write('<script src="cordova.ios/cordova.js"></script>');
-     document.write('<script src="sqlite.ios/SQLitePlugin.js"></script>');
-}else{
-	alert("ripple");
-	document.write('<script src="ripple.ios/cordova.js"></script>');
     document.write('<script src="sqlite.ios/SQLitePlugin.js"></script>');
-
+}else{
+	//alert("ripple");
+	//document.write('<script src="ripple.ios/cordova.js"></script>');
+    //document.write('<script src="sqlite.ios/SQLitePlugin.js"></script>');
+//	 document.write('<script src="cordova/cordova.js"></script>');
+//	 document.write('<script src="sqlite.android/SQLitePlugin.js"></script>');
+	 
 }
