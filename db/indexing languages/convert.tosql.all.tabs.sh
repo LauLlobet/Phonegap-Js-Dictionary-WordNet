1d8dd64581a6473a036f91de28615cc9d652fc97
Lau Llobet Payas <lau.llobet.payas@gmail.com>
2013-10-02 11:39:07 +0200

    0.40DbVerbsLangs [3]  script que carrega tots els lema a la base de dades     mkdir  all_lang_tab , all_lang_tab.sql     ./convert.tosql.all.tabs.sh     all_lang_tab sera els *.tab del zip de tots els wordnets wn_s

---
+++ db/indexing languages/convert.tosql.all.tabs.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+cd all_lang_tab/
+FILES=*
+for f in $FILES
+do
+  echo "Processing $f file..."
+  lang=$(echo "$f" | sed-4.2 -r "s:wn-data-(...).*:\1:")
+  echo "lang: $lang"
+  out="$f.sql"
+  ../convert.lang_tab.sh $lang $f > ../all_lang_tab.sql/$out
+  sqlite3 ../new_indexed_lexitree.db < ../all_lang_tab.sql/$out
+done
+cd ..
